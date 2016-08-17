b61b76a19b01a59a1fb54da032fe4d2b3674d01e
Sapphire9898 <yueyangzou@gmail.com>
2016-08-17 19:22:22 -0400

    PhotoGallry

---
+++ app/src/main/java/com/example/yueyangzou/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,54 @@
+package com.example.yueyangzou.photogallery;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.IOException;
+
+/**
+ * Created by yueyangzou on 16/8/16.
+ */
+public class PhotoGalleryFragment extends Fragment{
+    private static final String TAG = "PhotoGalleryFragment";
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemsTask().execute();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mPhotoRecyclerView = (RecyclerView)v.findViewById(R.id.fragment_photo_gallery_recyclerview);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrlString("http://www.bignerdranch.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            }
+            catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
+    }
+}
