de18bf7c3ff5f8db00c609064869923a7b0fe269
Sapphire9898 <yueyangzou@gmail.com>
2016-08-17 21:35:53 -0400

    Get JSON from Flickr

--- app/src/main/java/com/example/yueyangzou/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/yueyangzou/photogallery/PhotoGalleryFragment.java
@@ -41,13 +41,14 @@ public class PhotoGalleryFragment extends Fragment{
     private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
-            try {
-                String result = new FlickrFetchr().getUrlString("http://www.bignerdranch.com");
-                Log.i(TAG, "Fetched contents of URL: " + result);
-            }
-            catch (IOException ioe) {
-                Log.e(TAG, "Failed to fetch URL: ", ioe);
-            }
+//            try {
+//                String result = new FlickrFetchr().getUrlString("http://www.google.com");
+//                Log.i(TAG, "Fetched contents of URL: " + result);
+//            }
+//            catch (IOException ioe) {
+//                Log.e(TAG, "Failed to fetch URL: ", ioe);
+//            }
+            new FlickrFetchr().fetchItems();
             return null;
         }
     }
